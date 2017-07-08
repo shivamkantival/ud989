c2a07d4cac76cbd35105c0b4a196e2e6d23b3de2
shivamkantival <shivam.kantival@yahoo.co.in>
2017-07-08 15:29:48 +0530

    final multi

--- multiple cats with list/multiCats.js
+++ multiple cats with list/multiCats.js
@@ -18,7 +18,8 @@ showCat = (function() {
 	return function(catNum) {
 		var imageElement = document.getElementById("display-image");
 		imageElement.setAttribute("src","cat"+catNum+".jpg");
-		var textPart = document.getElementById("display-count")
+		// var textPart = document.getElementById("display-count");
+		var textPart = imageElement.nextElementSibling;
 		counter[catNum-1].incCount();
 		textPart.innerHTML = counter[catNum-1].getCount() + "";
 		console.log(counter[catNum-1].getCount());
